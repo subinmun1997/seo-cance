a6d00fcb25823bf48e26968c3ef9d111f0c5a022
subinmun1997 <qzxy812@gmail.com>
2022-12-31 13:02:42 +0900

    Init project

---
+++ src/main/java/com/trip/seocance/SeoCanceApplication.java
@@ -0,0 +1,13 @@
+package com.trip.seocance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeoCanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeoCanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trip/seocance/SeoCanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trip.seocance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeoCanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
